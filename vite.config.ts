a18e441336d4726182e02114c98ca4e64e3c5bec
Anderson Foschiera <foschieraanderson@gmail.com>
2026-06-28 03:03:48 -0300

    feat: Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [vue(), VueDevTools(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
