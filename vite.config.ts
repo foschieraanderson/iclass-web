6e0e28fba02d8ffc2e755c238c36c73b7bd5c624
Anderson Foschiera <foschieraanderson@gmail.com>
2026-06-29 16:26:06 -0300

    feat: Add CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? '/',
   plugins: [vue(), VueDevTools(), tailwindcss()],
   resolve: {
     alias: {
